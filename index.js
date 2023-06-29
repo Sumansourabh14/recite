f8e8647d6f9a99b4f0c512cf6202b6c8d00d3c76
Suman Sourabh <suman.sourabh@ansrsource.com>
2023-06-29 20:53:03 +0530

    added cors

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const quoteRoute = require("./routes/QuoteRoute");
+const cors = require("cors");
 
 const app = express();
 dotenv.config();
 
-const port = process.env.PORT;
-
+app.use(cors({ origin: process.env.FRONTEND_URI }));
 app.use("/", quoteRoute);
+
+const port = process.env.PORT;
 
 app.listen(port, () => {
   console.log(`listening on http://localhost:${port} `);
